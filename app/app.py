9bdfc1f87f826ef8e0cf1563e9fe2a0fa61fb194
knyaz7 <106472064+knyaz7@users.noreply.github.com>
2024-09-19 18:00:15 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,11 +4,11 @@ from db import engine, Base
 
 app = FastAPI()
 
-# Подключаем роутер для работы с вебсокетами и аудиозаметками
-app.include_router(audio_notes_router)
-
 
 @app.on_event("startup")
 async def startup():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+# Подключаем роутер для работы с вебсокетами и аудиозаметками
+app.include_router(audio_notes_router)
